059392b36c3b985ec0b349f8a96cc4ea9af528af
gigglesbw4 <blakewrege1@gmail.com>
2016-03-15 05:05:01 -0400

    A3 ALMOST DONE

---
+++ A3/A3P2.c
@@ -0,0 +1,54 @@
+
+/*************************************************
+* Course: CS 4540 – Spring 2016
+* Assignment 3 - Problem 1
+* Name: Blake Wrege
+* E-mail: blake.j.wrege@wmich.edu
+* Submitted: 3/14/2016
+*************************************************/
+
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <pthread.h>
+
+
+
+double sum;
+int count = 0;
+int n = 268435456;
+int Narray[268435456];
+
+
+
+
+int main() {
+	struct timespec start, finish;
+	double elapsed;
+	clock_gettime(CLOCK_MONOTONIC, &start);
+
+    for (int j=0;j<n;j++){
+        Narray[j] =  j;
+    
+	}
+
+    for (int i=0;i<n;i++){
+        sum=sum+Narray[i];
+    
+	}
+printf("%lf\n",sum);
+	
+	clock_gettime(CLOCK_MONOTONIC, &finish);
+	elapsed = (finish.tv_sec - start.tv_sec);
+	elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
+	printf(" %f", elapsed);
+	return 0;
+}
+
+
