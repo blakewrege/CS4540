6a7d893beea674a0a50490963c28d4c1ca7fffa1
gigglesbw4 <blakewrege1@gmail.com>
2016-04-22 16:20:05 -0400

    fixed makefile

--- A6/run-module.sh
+++ A6/run-module.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 #Creates, loads, and removes kernel module
 #NOTE: run as sudo
-sudo dmesg -c
+dmesg -c
 make
-sudo insmod ProjectA6.ko
-sudo rmmod ProjectA6
-sudo dmesg > dmesg-output.txt
-chown blake dmesg-output.txt
+insmod ProjectA6.ko
+rmmod ProjectA6
+dmesg > dmesg-output.txt
 cat dmesg-output.txt
 
